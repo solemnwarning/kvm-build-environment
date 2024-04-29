7909be742ab3b1928a617eb20f62c7825232b415
Daniel Collins <solemnwarning@solemnwarning.net>
2024-04-29 20:43:54 +0100

    Initial repo structure and SnapPass server VM.

---
+++ build-infrastructure/build-infrastructure.main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  alias = "vmhost01"
+  uri = "qemu+ssh://root@vmhost01.lan.solemnwarning.net/system?sshauth=privkey"
+}
+
+module "snappass" {
+  source    = "./snappass-server-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost01
+  }
+
+  hostname = "snappass"
+  domain = "build.solemnwarning.net"
+
+  ip_and_prefix = "172.24.134.8/26"
+  gateway = "172.24.134.1"
+  dns_server = "172.24.128.1"
+}
+
+output "snappass_root_password" {
+    value = "${module.snappass.root_password}"
+    sensitive = true
+}
+
+output "snappass_https_cert" {
+    value = "${module.snappass.https_cert}"
+}
