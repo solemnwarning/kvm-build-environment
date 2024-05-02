170cfaa0858f26286a610281220e743d8c394c0a
Daniel Collins <solemnwarning@solemnwarning.net>
2024-05-03 00:03:01 +0100

    Deploy Windows build agents.

--- build-agents/build-agents.main.tf
+++ build-agents/build-agents.main.tf
@@ -12,6 +12,16 @@ provider "libvirt" {
 }
 
 provider "libvirt" {
+  alias = "vmhost02"
+  uri = "qemu+ssh://root@vmhost02.lan.solemnwarning.net/system?sshauth=privkey"
+}
+
+provider "libvirt" {
+  alias = "vmhost03"
+  uri = "qemu+ssh://root@vmhost03.lan.solemnwarning.net/system?sshauth=privkey"
+}
+
+provider "libvirt" {
   alias = "vmhost04"
   # uri = "qemu+ssh://root@vmhost04.lan.solemnwarning.net/system?sshauth=privkey"
   uri = "qemu:///system"
@@ -44,4 +54,71 @@ module "ipxwrapper_test_vmhost04" {
 output "ipxwrapper_test_vmhost04_root_password" {
     value = "${module.ipxwrapper_test_vmhost04.root_password}"
     sensitive = true
+}
+
+module "windows_build_vmhost01" {
+  source    = "./windows-build-agent-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost01
+  }
+
+  domain = "build.solemnwarning.net"
+
+  memory = 16384
+  vcpu   = 8
+  spawn  = 1
+}
+
+module "windows_build_vmhost02" {
+  source    = "./windows-build-agent-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost02
+  }
+
+  domain = "build.solemnwarning.net"
+
+  memory = 16384
+  vcpu   = 8
+  spawn  = 1
+}
+
+module "windows_build_vmhost03" {
+  source    = "./windows-build-agent-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost03
+  }
+
+  domain = "build.solemnwarning.net"
+
+  memory = 16384
+  vcpu   = 8
+  spawn  = 1
+}
+
+module "windows_build_vmhost04_a" {
+  source    = "./windows-build-agent-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost04
+  }
+
+  hostname_suffix = "-a"
+  domain = "build.solemnwarning.net"
+
+  memory = 49152
+  vcpu   = 24
+  spawn  = 1
+}
+
+module "windows_build_vmhost04_b" {
+  source    = "./windows-build-agent-deploy/"
+  providers = {
+    libvirt = libvirt.vmhost04
+  }
+
+  hostname_suffix = "-b"
+  domain = "build.solemnwarning.net"
+
+  memory = 49152
+  vcpu   = 24
+  spawn  = 1
 }
---
+++ build-agents/windows-build-agent-deploy/windows-build-agent.main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+    }
+  }
+}
+
+data "terraform_remote_state" "build_infrastructure" {
+  backend = "local"
+
+  config = {
+    path = "${abspath(path.root)}/../build-infrastructure/terraform.tfstate"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+locals {
+  hostname = "windows-build${ var.hostname_suffix }-${ random_id.suffix.hex }"
+}
+
+data "local_file" "image_version" {
+  filename = "${ path.root }/windows-build-agent-image/builds/latest-version"
+}
+
+locals {
+  image_version = chomp(data.local_file.image_version.content)
+  image_path    = "${ path.root }/windows-build-agent-image/builds/${ local.image_version }/windows-build-agent.qcow2"
+}
+
+resource "libvirt_volume" "root" {
+  name   = "${ local.hostname }.${ var.domain }_root.qcow2"
+  pool   = var.storage_pool
+  source = local.image_path
+  format = "qcow2"
+}
+
+resource "libvirt_cloudinit_disk" "cloud_init" {
+  name = "${ local.hostname }.${ var.domain }_cloud-init.iso"
+  pool = var.storage_pool
+
+  # Ensure user_data has DOS line endings for our horrible batch script to be
+  # able to parse it correctly.
+  user_data  = replace(replace(
+    templatefile("${ path.module }/windows-build-agent.user-data.tftpl", {
+      hostname = "${ local.hostname }"
+      spawn = var.spawn
+      git_cache_https_cert = data.terraform_remote_state.build_infrastructure.outputs.git_cache_https_cert
+  }), "\r", ""), "\n", "\r\n")
+}
+
+resource "libvirt_domain" "windows_build_agent" {
+  name    = "${ local.hostname }.${ var.domain }"
+  memory  = var.memory
+  vcpu    = var.vcpu
+  running = false
+
+  # Destroy the VM when replacing the disk, otherwise it may be left running
+  # and the disk changed out from under it.
+  lifecycle {
+    replace_triggered_by = [
+      libvirt_volume.root.id,
+    ]
+  }
+
+  cloudinit = "${libvirt_cloudinit_disk.cloud_init.id}"
+
+  network_interface {
+    bridge = "dmz-build"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.root.id}"
+  }
+}
