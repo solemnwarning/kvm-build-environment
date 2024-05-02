939b2ce32616eae50f33fc4fa9b919ca18581833
Daniel Collins <solemnwarning@solemnwarning.net>
2024-05-03 00:00:42 +0100

    Don't deploy snappass server.

--- build-infrastructure/build-infrastructure.main.tf
+++ build-infrastructure/build-infrastructure.main.tf
@@ -11,35 +11,35 @@ provider "libvirt" {
   uri = "qemu+ssh://root@vmhost01.lan.solemnwarning.net/system?sshauth=privkey"
 }
 
-module "snappass" {
-  source    = "./snappass-server-deploy/"
-  providers = {
-    libvirt = libvirt.vmhost01
-  }
+# module "snappass" {
+#   source    = "./snappass-server-deploy/"
+#   providers = {
+#     libvirt = libvirt.vmhost01
+#   }
+#
+#   hostname = "snappass"
+#   domain = "build.solemnwarning.net"
+#
+#   ip_and_prefix = "172.24.134.8/26"
+#   gateway = "172.24.134.1"
+#   dns_server = "172.24.128.1"
+# }
 
-  hostname = "snappass"
-  domain = "build.solemnwarning.net"
+# output "snappass_root_password" {
+#     value = "${module.snappass.root_password}"
+#     sensitive = true
+# }
 
-  ip_and_prefix = "172.24.134.8/26"
-  gateway = "172.24.134.1"
-  dns_server = "172.24.128.1"
-}
+# output "snappass_https_cert" {
+#     value = "${module.snappass.https_cert}"
+# }
 
-output "snappass_root_password" {
-    value = "${module.snappass.root_password}"
-    sensitive = true
-}
-
-output "snappass_https_cert" {
-    value = "${module.snappass.https_cert}"
-}
-
-resource "local_file" "snappass_https_cert" {
-    filename = "${ path.root }/snappass.build.solemnwarning.net.crt"
-    file_permission = "0644"
-
-    content = "${module.snappass.https_cert}"
-}
+# resource "local_file" "snappass_https_cert" {
+#     filename = "${ path.root }/snappass.build.solemnwarning.net.crt"
+#     file_permission = "0644"
+#
+#     content = "${module.snappass.https_cert}"
+# }
 
 module "vcpkg_cache" {
   source    = "./vcpkg-cache-server-deploy/"
