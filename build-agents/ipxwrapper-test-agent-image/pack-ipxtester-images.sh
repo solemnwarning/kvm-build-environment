6bf66401ac4682ca186d4feb0b49174486771790
Daniel Collins <solemnwarning@solemnwarning.net>
2026-09-23 22:06:49 +0100

    Update ipxtester, Windows 10 test images.

--- build-agents/ipxwrapper-test-agent-image/pack-ipxtester-images.sh
+++ build-agents/ipxwrapper-test-agent-image/pack-ipxtester-images.sh
@@ -4,14 +4,14 @@
 
 set -eE
 
-DISK_SIZE="120G"
+DISK_SIZE="200G"
 
 IPXTESTER_IMAGES=(
     "ipxtester-images/ipxtest-director-2024-07-03"
     "ipxtester-images/ipxtest-winXPx86-2026-08-04"
     "ipxtester-images/ipxtest-win7x64-2023-09-12"
     "ipxtester-images/ipxtest-win81x86-2023-09-11"
-    "ipxtester-images/ipxtest-win10x64-2026-08-02"
+    "ipxtester-images/ipxtest-win10x64-2026-09-19"
     "ipxtester-images/ipxtest-win11x64-2025-07-31"
     "ipxtester-images/ipxtest-win98-2024-11-03"
 )
